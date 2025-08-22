ee159c3b5df9df170ed60a035b04e1893c3b6324
Christopher Peterson <teambuilderapp2@gmail.com>
2025-08-22 10:00:20 -0400

    Added the main App footer. Made the Font Awesome import global.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../Assets/Footer.css';
 
 //This is the main App Footer.
 //Stateless components do not need access to any state or functionality. 
@@ -6,7 +7,32 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="App">
-        This is the FOOTER
+        <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>
+              My React App UIUX portfolio demo by software engineer, Christopher
+              Peterson.
+              <br /><b>Email:</b> cpeterson.software.engineering@gmail.com
+              <br /><b>Phone:</b> (517) 297-5674 
+              <br /><a href="https://mern-demo-49es.onrender.com/">Here's my MERN Demo!</a><br />
+              <a href="https://vue-demo-t89m.onrender.com/#/">Here's my Vue Demo!</a><br /><a
+                class="icon"
+                href="https://github.com/TeamBuilderApp?tab=repositories"
+                ><i class="fab fa-github"></i
+              ></a>
+              <a
+                class="icon"
+                href="https://www.linkedin.com/in/christopher-peterson-86728a303"
+                ><i class="fab fa-linkedin-in"></i
+              ></a>
+            </p>
+          </div>
+          <div class="column has-text-right"></div>
+        </div>
+      </div>
+    </footer>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Assets/index.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
